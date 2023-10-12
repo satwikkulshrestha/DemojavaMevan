347bebcfd69b4ac34afd9b4017ce6f6a50276a99
Satwik Kulshrestha <satwikkulshrestha@gmail.com>
2023-10-12 22:59:16 +0530

    First commit

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,25 @@
+package mypkg;
+
+import java.util.Scanner;
+
+public class MyClass {
+	public int arearect(int l, int b)
+	{
+		return (l*b);
+	}
+	
+
+	public static void main(String[] args) {
+		MyClass ob = new MyClass();
+		int l;
+		int b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter length : ");
+		l=sc.nextInt();
+		System.out.println("Enter Breadth : ");
+		b=sc.nextInt();
+		System.out.println("Area of rectangle is : "+ob.arearect(l,b) );
+	
+	}
+
+}
